41c877d3c533227ff1c34fc1f56cac81e2d049a9
Teja Sasank Pagolu <sasankofficial@gmail.,com>
2018-11-21 10:10:30 +0530

    First Commit

---
+++ Python logic/mohits_problem.py
@@ -0,0 +1,40 @@
+def mohitifier(source_list):
+    occupied_list = []
+    difference_list = []
+    max_difference = 0
+
+    for i in range(0, len(source_list)):
+        if(source_list[i] == 1):
+            occupied_list.append(i)
+
+    if(len(occupied_list)==0):       
+       source_list[0]=1
+    elif(len(occupied_list)==1):
+        source_list[len(source_list)-1]=1
+    else:
+        for j in range(0, len(occupied_list)-1):
+            difference = abs(occupied_list[j]-occupied_list[j+1])
+            difference_list.append(difference)
+        
+        max_difference = max(difference_list)
+        for k in range(0, len(occupied_list)-1):
+            difference = abs(occupied_list[k]-occupied_list[k+1])
+            if(difference == max_difference):
+                source_list[(occupied_list[k]+(difference//2))]=1
+                
+    print(source_list)
+    
+mohitifier([1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1])
+
+
+
+
+
+
+
+
+
+
+
+
+
